e9a3bf94b9678b3f79c5dff32354cf2271b21de0
mctods <siregarandi14@gmail.com>
2023-10-24 05:40:54 +0700

    Folder Upload

---
+++ app/src/main/java/com/example/andisiregar/informasi.kt
@@ -0,0 +1,11 @@
+package com.example.andisiregar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class informasi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_informasi)
+    }
+}
---
+++ app/src/main/java/com/example/andisiregar/jadwal.kt
@@ -0,0 +1,11 @@
+package com.example.andisiregar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class jadwal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jadwal)
+    }
+}
